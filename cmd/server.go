2a78bdc7f0bf6cd799875344955a55c6e639140a
Ahsan Kazmi <ahsan.r.kazmi@gmail.com>
2021-03-11 19:42:10 -0800

    Adding API to get report results for a provided search term, and adding more reports.

--- cmd/server.go
+++ cmd/server.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-	"../internal"
 	"bufio"
 	"database/sql"
 	"encoding/json"
@@ -12,6 +11,7 @@ import (
 	"log"
 	"net/http"
 	"os"
+	"segmed-demo-back-end/internal"
 	"strconv"
 )
 
@@ -23,17 +23,32 @@ const (
 	dbName     = "segmed"
 
 	elasticSearchUrl = "http://localhost:9200"
-	indexName = "segmed"
-	typeName = "report"
+	indexName        = "segmed"
+	typeName         = "report"
 )
 
 type Report struct {
-	Id int64		`json:"id"`
+	Id       int64  `json:"id"`
 	FileName string `json:"fileName"`
-	Name string 	`json:"name"`
-	Author string	`json:"author"`
-	Text string 	`json:"text"`
+	Name     string `json:"name"`
+	Author   string `json:"author"`
+	Synopsis string `json:"synopsis"`
+	Text     string `json:"text"`
 }
+
+type EsReport struct {
+	Report
+	Score float64 `json:"score"`
+}
+
+type Query struct {
+	Query struct {
+		Match struct {
+			Text string `json:"text"`
+		} `json:"match"`
+	} `json:"query"`
+}
+
 const MaxMultipartFormMemory = 32 << 20
 const StaticTextFileLocation = "./web/static/text"
 
@@ -58,9 +73,10 @@ func main() {
 	router.MaxMultipartMemory = MaxMultipartFormMemory
 
 	router.Static("/static", "web/static/")
-	fileApiV1 := router.Group("/api/v1/file")
+	fileApiV1 := router.Group("/api/v1/report")
 	{
 		fileApiV1.GET("/getAllReportInfo", HandleGetAllReportInfo)
+		fileApiV1.GET("/getReportInfo", HandleGetReportSearchResults)
 	}
 
 	// On startup go through each of the static text files and see if they are already present in the report table.
@@ -69,7 +85,6 @@ func main() {
 
 	log.Fatal(router.Run(":8081"))
 }
-
 
 func AddAllReportsToDatabaseAndElasticSearch() {
 	files, err := ioutil.ReadDir(StaticTextFileLocation)
@@ -82,6 +97,23 @@ func AddAllReportsToDatabaseAndElasticSearch() {
 
 func AddReportToDatabaseAndElasticSearch(fileName string) {
 	log.Printf("Adding report with fileName = %s to database and elastic search.\n", fileName)
+
+	// Check that a connection to the database can be opened.
+	err := db.Ping()
+	internal.HandleError(err)
+
+	row := db.QueryRow("SELECT COUNT(*) FROM report WHERE file_name = $1", fileName)
+
+	var count int64
+	err = row.Scan(&count)
+	internal.HandleError(err)
+
+	if count != 0 {
+		// Do not try to add the report again since it has already been added to the database (and elastic search).
+		// TODO: Perhaps add a check that it is also in elastic search, and if not then add it.
+		log.Printf("The report already exists, skipping further processing.\n")
+		return
+	}
 
 	file, err := os.Open(StaticTextFileLocation + "/" + fileName)
 	if err != nil {
@@ -111,7 +143,7 @@ func AddReportToDatabaseAndElasticSearch(fileName string) {
 			author = internal.ExtractValueFromLine(line, "Author:")
 		}
 
-		text += line
+		text += line + "\n"
 		counter += 1
 	}
 
@@ -119,21 +151,10 @@ func AddReportToDatabaseAndElasticSearch(fileName string) {
 		internal.HandleError(err)
 	}
 
-	// Check that a connection to the database can be opened.
-	err = db.Ping()
+	// Add the report to the database.
+	insertStatement := "INSERT INTO report(name, author, file_name, synopsis, text) VALUES($1, $2, $3, $4, $5)"
+	_, err = db.Exec(insertStatement, name, author, fileName, synopsis, text)
 	internal.HandleError(err)
-
-	row := db.QueryRow("SELECT COUNT(*) FROM report WHERE file_name = $1", fileName)
-
-	var count int64
-	err = row.Scan(&count)
-	internal.HandleError(err)
-
-	if count == 0 {
-		insertStatement := "INSERT INTO report(name, author, file_name, synopsis) VALUES($1, $2, $3, $4)"
-		_, err = db.Exec(insertStatement, name, author, fileName, synopsis)
-		internal.HandleError(err)
-	}
 
 	row = db.QueryRow("SELECT id FROM report WHERE file_name = $1", fileName)
 	var id int64
@@ -145,21 +166,22 @@ func AddReportToDatabaseAndElasticSearch(fileName string) {
 		fileName,
 		name,
 		author,
+		synopsis,
 		text,
 	}
 
+	// Add the report to the elastic search.
 	jsonObject, err := json.Marshal(report)
 	internal.HandleError(err)
-	internal.DoHttpPutRequest(jsonObject, elasticSearchUrl + "/" + indexName + "/" + typeName + "/" + strconv.FormatInt(id, 10)  + "?pretty")
+	internal.DoHttpPutRequest(jsonObject, elasticSearchUrl+"/"+indexName+"/"+typeName+"/"+strconv.FormatInt(id, 10)+"?pretty")
 }
-
 
 func HandleGetAllReportInfo(c *gin.Context) {
 	log.Println("HandleGetAllReportInfo attempting to get the information for all reports.")
 
 	defer func() {
 		if r := recover(); r != nil {
-			log.Println("Recovered in HandleGetAllReportInfo after trying to get all reports. The following " +
+			log.Println("Recovered in HandleGetAllReportInfo after trying to get all reports. The following "+
 				"error was encountered: ", r)
 
 			c.String(http.StatusInternalServerError, fmt.Sprintf("Error in getting report information."))
@@ -175,6 +197,15 @@ func HandleGetAllReportInfo(c *gin.Context) {
 	rows, err := db.Query("SELECT id, name, author, file_name, synopsis FROM report ORDER by name ASC")
 	internal.HandleError(err)
 
+	reportList := buildReportListFromQueryResult(rows)
+
+	log.Println("Returning response with all report information.")
+	c.JSON(http.StatusOK, reportList)
+
+	return
+}
+
+func buildReportListFromQueryResult(rows *sql.Rows) []Report {
 	defer rows.Close()
 
 	var reportList []Report
@@ -184,7 +215,7 @@ func HandleGetAllReportInfo(c *gin.Context) {
 		var author string
 		var fileName string
 		var synopsis string
-		err = rows.Scan(&id, &name, &author, &fileName, &synopsis)
+		err := rows.Scan(&id, &name, &author, &fileName, &synopsis)
 		internal.HandleError(err)
 
 		report := Report{
@@ -193,16 +224,92 @@ func HandleGetAllReportInfo(c *gin.Context) {
 			author,
 			fileName,
 			synopsis,
+			"", // The full text is not returned in this call for efficiency. It will be returned by the API for a single report.
 		}
 
 		reportList = append(reportList, report)
 	}
 
-	err = rows.Err()
+	err := rows.Err()
 	internal.HandleError(err)
+
+	return reportList
+}
+
+func HandleGetReportSearchResults(c *gin.Context) {
+
+	defer func() {
+		if r := recover(); r != nil {
+			log.Println("Recovered in HandleGetReportSearchResults after trying to get report search results."+
+				" The following error was encountered: ", r)
+
+			c.String(http.StatusInternalServerError, fmt.Sprintf("There was an error in getting report search results."))
+			return
+		}
+	}()
+
+	params := c.Request.URL.Query()
+	searchTerm := params.Get("searchTerm")
+
+	if searchTerm == "" {
+		HandleGetAllReportInfo(c)
+		return
+	}
+
+	log.Printf("HandleGetReportSearchResults attempting to get report search results for searchTerm = %s.\n",
+		searchTerm)
+
+	// Create the body for the elastic search query
+	var query Query
+	query.Query.Match.Text = searchTerm
+	jsonObject, err := json.Marshal(query)
+	internal.HandleError(err)
+
+	resp := internal.DoHttpGetRequestWithBody(jsonObject, elasticSearchUrl+"/"+indexName+"/"+typeName+"/_search?pretty")
+
+	bodyBytes, err := ioutil.ReadAll(resp.Body)
+	internal.HandleError(err)
+	var reportList []EsReport
+	if resp.StatusCode == http.StatusOK {
+		var jsonObject map[string]interface{}
+		err := json.Unmarshal(bodyBytes, &jsonObject)
+		internal.HandleError(err)
+
+		log.Printf(
+			"Recovered %d hits; took: %dms\n",
+			int(jsonObject["hits"].(map[string]interface{})["total"].(map[string]interface{})["value"].(float64)),
+			int(jsonObject["took"].(float64)),
+		)
+
+		for _, hit := range jsonObject["hits"].(map[string]interface{})["hits"].([]interface{}) {
+			source := hit.(map[string]interface{})["_source"]
+
+			report := Report{
+				int64(source.(map[string]interface{})["id"].(float64)),
+				source.(map[string]interface{})["fileName"].(string),
+				source.(map[string]interface{})["name"].(string),
+				source.(map[string]interface{})["author"].(string),
+				source.(map[string]interface{})["synopsis"].(string),
+				"",
+			}
+
+			esReport := EsReport{
+				report,
+				hit.(map[string]interface{})["_score"].(float64),
+			}
+
+			reportList = append(reportList, esReport)
+		}
+
+	} else {
+		body := string(bodyBytes)
+		errMsg := "There was an error in getting report search results."
+		log.Printf(errMsg+"%s", body)
+		c.String(http.StatusInternalServerError, errMsg)
+		return
+	}
 
 	log.Println("Returning response with all report information.")
 	c.JSON(http.StatusOK, reportList)
-
 	return
 }
--- internal/util.go
+++ internal/util.go
@@ -16,10 +16,10 @@ func HandleError(err error) {
 
 // Allow all origins access, since the back-end application will not be accessible by the outside world.
 // https://stackoverflow.com/questions/29418478/go-gin-framework-cors
-func HandleCorsMiddleware(c *gin.Context)  {
+func HandleCorsMiddleware(c *gin.Context) {
 	c.Writer.Header().Set("Access-Control-Allow-Origin", "*")
 	c.Writer.Header().Set("Access-Control-Allow-Credentials", "true")
-	c.Writer.Header().Set("Access-Control-Allow-Headers", "Content-Type, Content-Length, " +
+	c.Writer.Header().Set("Access-Control-Allow-Headers", "Content-Type, Content-Length, "+
 		"Accept-Encoding, X-CSRF-Token, Authorization, accept, origin, Cache-Control, X-Requested-With")
 	c.Writer.Header().Set("Access-Control-Allow-Methods", "POST, OPTIONS, GET, PUT")
 
@@ -46,14 +46,28 @@ func DoHttpPutRequest(body []byte, url string) {
 	log.Printf("DoHttpPutRequest attempting to make a PUT request to url %s.", url)
 	client := &http.Client{}
 
-	// set the HTTP method, url, and request body
 	req, err := http.NewRequest(http.MethodPut, url, bytes.NewBuffer(body))
 	HandleError(err)
 
-	// set the request header Content-Type for json
 	req.Header.Set("Content-Type", "application/json")
 	resp, err := client.Do(req)
 
 	HandleError(err)
 	log.Printf("Received response with Staus = %s.\n", resp.Status)
+}
+
+func DoHttpGetRequestWithBody(body []byte, url string) *http.Response {
+	log.Printf("DoGetPutRequest attempting to make a GET request to url %s and body %s.", url, body)
+	client := &http.Client{}
+
+	req, err := http.NewRequest(http.MethodGet, url, bytes.NewBuffer(body))
+	HandleError(err)
+
+	req.Header.Set("Content-Type", "application/json")
+	resp, err := client.Do(req)
+
+	HandleError(err)
+	log.Printf("Received response with Staus = %s.\n", resp.Status)
+
+	return resp
 }
